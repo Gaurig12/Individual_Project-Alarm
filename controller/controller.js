6ec75bcadcfe2283695a99e077d9a3a86994263e
Gaurig12 <157452904+Gaurig12@users.noreply.github.com>
2024-02-14 11:07:12 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,46 +1,27 @@
 const model = require('../model/model');
+
 const controller = {
    
-    createEntry: async(req,res) => {
+    createData: async(req,res) => {
         console.log(req);
         try {
             const data = req.body;
-            console.log("info is"+""+data)
-            const newEntry = await model.createEntry(data);
-            res.status(201).json(newEntry);
-            console.log('work');
-            console.log('New entry created:', newEntry);
+            console.log(data)
+            const newdata = await model.createData(data);
+            res.status(201).json(newdata);
+            console.log('New data entered:', newdata);
         } catch (error) {
-            console.error('Error creating entry:',error);
+            console.error('Error creating data:', error);
             res.status(500).json({errorce: 'Server error'});
         }
-       
     },
 
-    getData: async(req,res) => {
+    readData: async(req,res) => {
         try {
-            const db_data = await model.getData();
-            res.json(db_data);
+            const dbdata = await model.readData();
+            res.json(dbdata);
         } catch (error) {
             res.status(500).json({errorccc: 'Server error'});
-        }
-    },
-
-   
-    editData: async(req,res) => {
-        const id = req.params.sensor_name;
-        const editDbInfo = req.body;
-        try {
-           const editedData = await model.editData(id, editDbInfo);
-           if(editedData){
-            res.json(editedData);
-            console.log('Data Updated successfully!')
-           } 
-           else {
-            res.status(404).json({error: ' data not found'});
-           }
-        } catch (error) {
-            res.status(500).json({error: 'Server error'});
         }
     }
 
