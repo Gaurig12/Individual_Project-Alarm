6b7d34a943902b2d154b34e91f204382b926c6d6
Gaurig12 <157452904+Gaurig12@users.noreply.github.com>
2024-02-14 11:09:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,15 @@ const http = require('http');
 const port = 3000;
 const fs = require('fs') 
 
-
 dotenv.config();
 app.use(express.json());
 
 app.use('/', Routes);
-
-app.get('/getData',function(req,res){ 
+app.get('/readData',function(req,res){ 
     res.sendFile(__dirname+'/index.html') 
 }) 
 
-app.listen(port, () => {
-    console.log(`server is listening in port ${port}`)
+app.listen(3000, () => {
+    console.log(`server is listening in port 3000`)
 })
 
