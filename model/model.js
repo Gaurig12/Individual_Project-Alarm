76f419e0a42dc955576e44a374310299f1ec3661
Gaurig12 <157452904+Gaurig12@users.noreply.github.com>
2024-02-14 11:08:05 +0530

    Update model.js

--- model/model.js
+++ model/model.js
@@ -1,9 +1,7 @@
 const pool = require('../config/db.config')
 
 const model = {
-
-    //create entry
-   createEntry: async(data) => {
+   createData: async(data) => {
         const {status, id, type , phase , level , project , well , sprovider , depth , indepth , systatus} = data;
         const query = 'INSERT INTO "Alarmdetails" (status, id, type , phase , level , project , well , sprovider , depth , indepth , systatus) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *';
         const values = [status, id, type , phase , level , project , well , sprovider , depth , indepth , systatus];
@@ -11,19 +9,11 @@ const model = {
         return result.rows[0];
     },
 
-    getData: async() => {
+    readData: async() => {
         const query = 'SELECT * FROM "Alarmdetails"';
         const result = await pool.query(query);
         return result.rows;
     },
-
-    /*editData: async(id, editDbInfo) => {
-        const {operator_low, operator_high} = editDbInfo;
-        const query = 'UPDATE "Alarmdetails" SET type = $1, phase = $2 WHERE id = $3 RETURNING *';
-        const values=[operator_low, operator_high, sensor_name];
-        const result = await pool.query(query, values);
-        return result.rows[0];
-    }*/
 }
 
 module.exports = model
