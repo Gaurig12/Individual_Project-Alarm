9e984901fb1309b588c3bb4a66853bbf1d5f5323
Gaurig12 <157452904+Gaurig12@users.noreply.github.com>
2024-02-14 11:08:53 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -2,24 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/controller');
 
-
-
-router.get('/getData', async(req,res) => {
+router.get('/readData', async(req,res) => {
     try {
-        await controller.getData(req,res);
+        await controller.readData(req,res);
     } catch (error) {
         console.log(error);
         res.status(500).json({errorrrr: 'Server error'});
-    }
-})
-/*
-router.put('/editData/:id', async(req,res) => {
-    try {
-        await controller.editData(req,res);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: 'Server error'});
-    }
-})*/
+    }})
 
 module.exports= router;
